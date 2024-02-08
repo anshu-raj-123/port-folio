7f1042bc697e486eb22dd4e6b1f2a92b67566d1f
Anshu <81515597+anshu-raj-123@users.noreply.github.com>
2024-02-08 14:48:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors"); // Importing cors module
+const cors = require("cors"); 
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const app = express();
-app.use(cors()); // Adding cors middleware to allow cross-origin requests
+app.use(cors()); 
 app.use(express.json());
 app.use("/", router);
 
-const port = process.env.PORT || 5000; // Using the port specified in environment variable or default to 5000
+const port = 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
 
 const contactEmail = nodemailer.createTransport({
